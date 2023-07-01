06bc92d84fbe9868697118ec79245297302eee9a
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2023-07-01 18:29:43 -0300

    comitar criação do meu primeiro programa

---
+++ 1 . programa simples/src/1 . programa simples.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : .c
+ Author      : Luiz
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Meu primeiro programa"); /* prints Meu primeiro programa */
+	return EXIT_SUCCESS;
+}

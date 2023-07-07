f59a8b0873d5e3ab6d1d2ee795cf97803b8c04ca
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2023-07-07 15:36:24 -0300

    comitar criação de programas-teste

---
+++ 2 . programa simples/src/2 . programa simples.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : .c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Ui ui ui ai ai ai!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}

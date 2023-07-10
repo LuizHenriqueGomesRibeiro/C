bd42b945f1f164ccb725d50d98b260ee18ff8ec7
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2023-07-10 15:40:50 -0300

    comitar criação do programa scanf

--- 2 . scanf/src/2 . scanf.c
+++ 2 . scanf/src/2 . scanf.c
@@ -10,8 +10,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(void) {
-	puts("Ui ui ui ai ai ai!!!"); /* prints !!!Hello World!!! */
+	setvbuf(stdout, NULL, _IONBF, 0);
+	int integer1;
+	int integer2;
+
+	printf("Este é um programa simples de teste de scanf\nEscreva um número inteiro:");
+
+	scanf("%d",&integer1);
+	printf("Escreva um segundo número inteiro:");
+
+	scanf("%d",&integer2);
+
+	int soma;
+
+	soma = integer1 + integer2;
+
+	printf("A soma dos dois números é: %d",soma);
 	return EXIT_SUCCESS;
 }
